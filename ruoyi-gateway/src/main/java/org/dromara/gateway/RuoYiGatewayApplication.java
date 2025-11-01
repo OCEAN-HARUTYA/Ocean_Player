4598c0e114abd4a432021d225a7bd4e3ece66d3f
Harutya <harutyakumiko@gmail.com>
2025-11-01 15:16:29 +0900

    Initial commit

---
+++ ruoyi-gateway/src/main/java/org/dromara/gateway/RuoYiGatewayApplication.java
@@ -0,0 +1,22 @@
+package org.dromara.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthContributorAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+
+/**
+ * 网关启动程序
+ *
+ * @author ruoyi
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceHealthContributorAutoConfiguration.class})
+public class RuoYiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(RuoYiGatewayApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  网关启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
